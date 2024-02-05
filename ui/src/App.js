699646f2a837ea765677bdb67496f7722203dee6
Nikola <npoptomo@asu.edu>
2024-02-04 23:35:08 -0700

    Task TG-45 for US-8

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import UserCredentials from './Components/UserCredentials';
+import ProjectSlugInput from './Components/ProjectSlugInput';
+import {
+  BrowserRouter as Router,
+  Routes, 
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <UserCredentials />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Routes> 
+            <Route exact path="/" element={<UserCredentials />} /> 
+            <Route path="/project-slug" element={<ProjectSlugInput />} /> 
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
