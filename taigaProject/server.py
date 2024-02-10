63c676429abad3d789545100b3b03ff89279396a
Karthik Vaida <jvaida@asu.edu>
2024-02-10 11:39:21 -0700

    Task TG-57 for US-10

--- taigaProject/server.py
+++ taigaProject/server.py
@@ -3,49 +3,32 @@ from taigaApi.authenticate import authenticate
 from taigaApi.project.getProjectBySlug import get_project_by_slug
 from taigaApi.task.getTaskHistory import get_task_history
 from taigaApi.task.getTasks import get_closed_tasks, get_all_tasks
+from flask_cors import CORS
 
 
 
 app = Flask(__name__)
-
-def _build_cors_preflight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
-    return response
-
-def _corsify_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
+CORS(app)
 
 @app.route("/")
 def sampleRoute():
-    if request.method == "OPTIONS": # CORS preflight
-        return _build_cors_preflight_response()
-    else:
-        response = _corsify_actual_response(jsonify({
-            "message": "Hello, World!"
-        }))
-        return response
+    response = jsonify({
+        "message": "Hello, World!"
+    })
+    return response
 
-@app.route("/authenticate", methods=["POST", "OPTIONS"])
+@app.route("/authenticate", methods=["POST"])
 def auth():
-    if request.method == "OPTIONS": # CORS preflight
-        return _build_cors_preflight_response()
     username = request.json['username']
     password = (request.json['password'])
     print("username",username)
     print("pswd",password)
     auth_token = authenticate(username,password)
     print(auth_token)
-    return _corsify_actual_response(jsonify({"token": auth_token, "status": "success"}))
+    return jsonify({"token": auth_token, "status": "success"})
 
-@app.route("/projectDetails", methods=["POST", "OPTIONS"])
+@app.route("/projectDetails", methods=["POST"])
 def projectDetails():
-    if request.method == "OPTIONS": # CORS preflight
-        return _build_cors_preflight_response()
-    # Extract the token from the Authorization header
     auth_header = request.headers.get('Authorization')
     token=''
     if auth_header and auth_header.startswith('Bearer '):
@@ -56,15 +39,12 @@ def projectDetails():
     projectSlug= request.json['projectSlug']
     project_info = get_project_by_slug(projectSlug,token)
     if project_info == None:
-        return _corsify_actual_response(jsonify({"status": "error", "message": "Project not found"}))
-    return _corsify_actual_response(jsonify({"data":project_info, "status": "success"}))
+        return jsonify({"status": "error", "message": "Project not found"})
+    return jsonify({"data":project_info, "status": "success"})
 
 
-@app.route("/cycleTime", methods=["POST", "OPTIONS"])
+@app.route("/cycleTime", methods=["POST"])
 def cycle_time():
-    if request.method == "OPTIONS":  # CORS preflight
-        return _build_cors_preflight_response()
-
     auth_header = request.headers.get('Authorization')
     token = ''
     if auth_header and auth_header.startswith('Bearer '):
@@ -78,9 +58,9 @@ def cycle_time():
     if closed_task == 0:
         return jsonify({"message": "No closed tasks found in the project"}), 404
     avg_cycle_time = round((cycle_time / closed_task), 2)
-    return _corsify_actual_response(jsonify({"avg_cycle_time": avg_cycle_time, "status": "success"}))
+    return jsonify({"avg_cycle_time": avg_cycle_time, "status": "success"})
 
 
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True, port=5000)
