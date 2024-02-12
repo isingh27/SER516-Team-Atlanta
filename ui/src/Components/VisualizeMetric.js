6f17f65e881456999f9d59eceb26580e45f35326
psing153 <psing153@asu.edu>
2024-02-11 21:40:34 -0700

    Task TG-59 for US-11

--- ui/src/Components/VisualizeMetric.js
+++ ui/src/Components/VisualizeMetric.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import MetricInput from "./MetricInput";
 
-function VisualizeMetric({ metricInput }) {
+function VisualizeMetric({ metricInput, metricData }) {
   //TODO: Implement State Management for getting the metricInput
 
   return (
-    <Container> 
+    <Container>
       <Row>
         <Col md={12}>
-          {visualizationType === 'line' && (
+          {metricInput === "cycleTime" && (
             // Placeholder - Integrate charting library compatible with React-Bootstrap
-            <h3>Line Chart Visualization (Placeholder)</h3> 
-          )}
-          {visualizationType === 'bar' && (
-            // Placeholder - Integrate charting library compatible with React-Bootstrap
-            <h3>Bar Chart Visualization (Placeholder)</h3> 
+            <h3>{metricData}</h3>
           )}
         </Col>
       </Row>
