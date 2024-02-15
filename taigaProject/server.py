02736c0d4b42e8f7b347214f4a8019890fa01712
psing153 <psing153@asu.edu>
2024-02-14 18:26:11 -0700

    Task TG-69 for US-6

--- taigaProject/server.py
+++ taigaProject/server.py
@@ -3,6 +3,9 @@ from taigaApi.authenticate import authenticate
 from taigaApi.project.getProjectBySlug import get_project_by_slug
 from taigaApi.task.getTaskHistory import get_task_history, get_task_cycle_times
 from taigaApi.task.getTasks import get_closed_tasks, get_all_tasks
+from taigaApi.sprint.getCurrentSprintID import get_current_sprint_id
+from taigaApi.sprint.getMilestoneStats import get_milestone_stats
+
 from flask_cors import CORS
 from datetime import datetime, timedelta
 
@@ -108,6 +111,46 @@ def cycle_time_per_task():
         })
     
     return jsonify({"data": response_data, "status": "success"})
+
+
+@app.route("/burndownChart", methods=["POST"])
+def burndown_chart():
+    auth_header = request.headers.get('Authorization')
+    token = ''
+    if auth_header and auth_header.startswith('Bearer '):
+        token = auth_header.split(" ")[1]
+    else:
+        return jsonify({"message": "Token is missing or invalid"}), 401
+
+    project_id = request.json['projectId']
+
+    # Call the function to get the current sprint ID
+    sprint_id = get_current_sprint_id(project_id, token)
+
+    if sprint_id is None:
+        return jsonify({"message": "No current sprint found"}), 404
+
+    # Call the function to get milestone stats using the sprint ID
+    milestone_stats = get_milestone_stats(sprint_id, token)
+
+    if milestone_stats is None:
+        return jsonify({"message": "Error fetching milestone stats"}), 500
+
+    # Add an "id" field to each item in the "days" list starting with 1
+    for index, day_data in enumerate(milestone_stats["days"], start=1):
+        day_data["id"] = index
+
+    # Modify the response to only include the necessary fields
+    response_data = {
+        "days": milestone_stats["days"],
+        "name": milestone_stats["name"],
+        "total_points": milestone_stats["total_points"]
+    }
+
+    return jsonify({"burndown_chart_data": response_data, "status": "success"})
+
+
+
 
 
 
