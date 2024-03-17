e69123f467fa5c18d5eb377e34ebf8b207f8fde2
isingh27 <isingh27@asu.edu>
2024-03-17 00:46:52 -0700

    Task TG-139 for US-131

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('App Component Renders Correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Team-Atlanta/i);
   expect(linkElement).toBeInTheDocument();
 });
