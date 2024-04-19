a3963ed305713d4b1a57a3e7e1c4220820abecbe
avijay48 <avijay48@asu.edu.in>
2024-04-19 14:00:55 -0700

    [TASK TG-263- for US TG-262] "added Redis cache container for burndown service"

--- burndown/taigaApi/getUserStory.py
+++ burndown/taigaApi/getUserStory.py
@@ -11,7 +11,9 @@ from fastapi import HTTPException
 
 # Load environment variables from .env file
 load_dotenv()
-r_userstory = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+#redis config for caching
+r_userstory = redis.StrictRedis(host='redis-container-burndown', port=6379, db=0)
 
 class UserStoryFetchingError(Exception):
     def __init__(self, status_code, reason):
